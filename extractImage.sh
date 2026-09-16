29562c59b611a964327136ad2162f352d36120f3
fritzthecap <41594868+fritzthecap@users.noreply.github.com>
2026-09-17 00:11:45 +0200

    Add files via upload
    Fixed target directory of image file and added command argument checks.

--- extractImage.sh
+++ extractImage.sh
@@ -5,19 +5,50 @@
 IMAGEFILENAME=image.png
 
 [ -z "$1" -o ! -f "$1" -o -z "$2" ] &&    {
-    echo "SYNTAX: $0 videoFile time [imageFilePath]" >&2
-    echo "Extracts an image from a video to default image file $IMAGEFILENAME" >&2
-    echo "Example:" >&2
-    echo "	$0 myvideo.mp4 1:23 title-image.png" >&2
+    echo "SYNTAX: $0 videoFile time [imagePathFile]" >&2
+    echo "	Extracts an image from a video to an image file." >&2
+    echo "	Example:" >&2
+    echo "		$0 myvideo.mp4 1:23" >&2
     echo "	would extract the key-frame image at or before minute 1 second 23" >&2
-    echo "	from myvideo.mp4 to title-image.png." >&2
+    echo "	from myvideo.mp4 to $IMAGEFILENAME in directory where myvideo.mp4 resides" >&2
     exit 1
 }
 
 video=$1
 time=$2
-image=${3:-$IMAGEFILENAME}
 
-ffmpeg -y \
+[ -f "$video" ] || {
+	echo "No such file: $video" >&2
+	exit 1
+}
+
+videoDirectory=`dirname \$video`	# resolves to "." when no path on file
+
+if [ -z "$3" ]
+then
+	image=$videoDirectory/$IMAGEFILENAME
+else
+	imageDirectory=`dirname \$3`
+	
+	if [ $imageDirectory -eq "." ]
+	then
+		image=$videoDirectory/$3
+	else
+		image=$3
+	fi
+fi
+
+videoDirectory=`dirname \$video`
+
+echo "Creating $image ..." >&2
+	
+ffmpeg -y -v error \
 	-ss $time -i $video -frames:v 1 \
 	-f image2 $image
+
+if [ "$?" -eq 0 ]
+then
+	echo "Created $image" >&2
+else
+	echo "Error! Exitcode was $?" >&2
+fi
